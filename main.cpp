55db085d80c99a5174198fe07859777b734170c5
svxezm <igorb.kuhl@gmail.com>
2025-08-24 17:31:55 -0300

    feat: print each line at a time instead of load all the data first

--- main.cpp
+++ main.cpp
@@ -4,13 +4,12 @@
 
 namespace fs = std::filesystem;
 
-std::ostringstream iterate_entries(
+void iterate_entries(
   fs::path path,
   unsigned int &directories,
   unsigned int &files,
   std::string prefix
 ) {
-  std::ostringstream tree;
   std::vector<fs::directory_entry> entries = {
     fs::directory_iterator(path), {}
   };
@@ -18,29 +17,25 @@ std::ostringstream iterate_entries(
   for (int i = 0; i < entries.size(); i++) {
     fs::directory_entry entry = entries[i];
     bool is_last = (i == entries.size() - 1);
-    std::ostringstream line;
 
-    line << prefix
+    std::cout
+         << prefix
          << (is_last ? "└── " : "├── ")
          << entry.path().filename().string()
          << "\n";
 
-    tree << line.str();
-
     if (entry.is_directory()) {
       directories++;
-      tree << iterate_entries(
+      iterate_entries(
         entry.path(),
         directories,
         files,
         prefix + (is_last ? "    " : "│   ")
-      ).str();
+      );
     } else {
       files++;
     }
   }
-
-  return tree;
 }
 
 int main(int argc, char *argv[]) {
@@ -48,16 +43,15 @@ int main(int argc, char *argv[]) {
   unsigned int directories = 1;
   unsigned int files = 0;
 
-  std::ostringstream tree = iterate_entries(entry_point, directories, files, "");
-  std::ostringstream counts;
+  std::cout << ".\n";
+  iterate_entries(entry_point, directories, files, "");
 
-  counts << directories << " directories";
-
-  if (files > 0) counts << ", " << files << " files";
-
-  std::cout << "." << std::endl;
-  std::cout << tree.str() << std::endl;
-  std::cout << counts.str() << std::endl;
+  std::cout
+       << "\n"
+       << directories
+       << " directories";
+  if (files > 0) std::cout << ", " << files << " files";
+  std::cout << "\n";
 
   return 0;
 }
